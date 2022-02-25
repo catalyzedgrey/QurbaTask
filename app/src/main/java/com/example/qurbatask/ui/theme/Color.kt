21bcbb02d89e0a8ed8f884f733fb68649f6c7edc
Grey <amr.hany1@outlook.com>
2022-02-25 11:04:59 +0200

    Changes to resources

--- app/src/main/java/com/example/qurbatask/ui/theme/Color.kt
+++ app/src/main/java/com/example/qurbatask/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.example.qurbatask.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
+val Purple200 = Color(0xFFFFFFFF)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
-val GradientLeft = Color(0xC89ED9E4)
-val GradientRight = Color(0xF1988AE4)
+val GradientLeft = Color(0xFFCFD9F9)
+val GradientRight = Color(0xFFEFCEF5)
+
+val TextBubble = Color(0xF7F8FAFF)
+val TextColor = Color(0xFF363636)
+val TextColorMuted = Color(0x80363636)

b552c21dc3343874ab03cab6ed23b71c6c92d934
Grey <amr.hany1@outlook.com>
2022-02-25 09:35:59 +0200

    Added Montserrat FontFamily

--- app/src/main/java/com/example/qurbatask/ui/theme/Type.kt
+++ app/src/main/java/com/example/qurbatask/ui/theme/Type.kt
@@ -1,28 +1,42 @@
 package com.example.qurbatask.ui.theme
 
+
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.qurbatask.R
+
+val montserratFamily = FontFamily(
+    Font(R.font.montserrat_light, FontWeight.Light),
+    Font(R.font.montserrat_bold, FontWeight.Bold),
+    Font(R.font.montserrat_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = montserratFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    h1 = TextStyle(fontFamily = montserratFamily),
+    h2 = TextStyle(fontFamily = montserratFamily),
+    h3 = TextStyle(fontFamily = montserratFamily),
+
+     //Other default text styles to override
+    button = TextStyle(
+        fontFamily = montserratFamily,
+    ),
+//    caption = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 12.sp
+//    )
+
 )
+
